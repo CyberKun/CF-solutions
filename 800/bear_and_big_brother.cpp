d28ddeee310c1273fd63c08ee59385853ed246c3
Aditya <24UEC244@lnmiit.ac.in>
2026-04-04 19:22:06 +0530

    fix

--- 800/bear_and_big_brother.cpp
+++ 800/bear_and_big_brother.cpp
@@ -4,8 +4,8 @@ using namespace std;
 int main() {
     int a,b;
     cin >> a >> b;
-    int count=0;
-    while(a<=b){
+    int count = 0;
+    while(a <= b){
         a=3*a;
         b=2*b;
         count++;
--- aaa.cpp
+++ aaa.cpp
@@ -20,8 +20,38 @@ int main() {
     while (t--) {
         ll n;
         cin >> n;
-        vector<ll> vec(n);
-        for (int i = 0; i < n; i++) cin >> vec[i];
+        vector<vector<ll>>adj(n+1);
+        for(int i = 2 ; i <= n ; i++) {
+            ll u;
+            cin >> u;
+            adj[u].push_back(i);
+            adj[i].push_back(u);
+        }
+        string s;
+        cin >> s;
+
+        vector<int> vis(n+1, 0);
+        ll ct = 0;
+        vector<ll>bal(n+1 , 0);
+
+        function<void(int, int)> dfs = [&](int u, int parent){
+            for (int v : adj[u]){
+                if (v == parent) continue;
+                if(s[v-1] == 'B') bal[v]++;
+                else bal[v]--;
+                dfs(v, u);
+            }
+            for(int v : adj[u]) {
+                if(v == parent) continue;
+                bal[u] += bal[v];
+            }
+        };
+        bal[1] = (s[0] == 'B' ? 1 : -1);
+        dfs(1,0);
+        for(int i = 1 ; i <= n ; i++) {
+            if(bal[i] == 0) ct++;
+        }
+        cout << ct << '\n';
     }
     return 0;
 }
